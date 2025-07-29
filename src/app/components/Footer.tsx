9cc725e12d3563dc879387d51198a2c43ced63d3
Bautista Di Santo <bautista.disanto@outsidecube.com>
2025-07-29 11:37:07 -0300

    change footer text

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="border-t border-gray-800">
       <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <p className="text-center text-gray-300">
-          © {new Date().getFullYear()} Juan. All rights reserved.
+          Juan Bautista Di Santo {new Date().getFullYear()}
         </p>
       </div>
     </footer>
